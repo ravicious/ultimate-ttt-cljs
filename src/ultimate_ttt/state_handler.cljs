a3104c333d258a6f4ea42e15070a9e29edfdefeb
Rafał Cieślak <ravicious@gmail.com>
2015-03-25 21:30:37 +0100

    Set active board after clicking on a cell

--- src/ultimate_ttt/state_handler.cljs
+++ src/ultimate_ttt/state_handler.cljs
@@ -12,7 +12,8 @@
 
 (defonce app-state (atom {:main-board (board-helper/init-board)
                           :current-owner 1
-                          :boards (init-boards)}))
+                          :boards (init-boards)
+                          :active-board nil}))
 
 (defn- choose-next-owner []
   (let [current-owner (:current-owner @app-state)]
@@ -28,7 +29,19 @@
         new-board (board-helper/set-cell board-record (first coordinates) (second coordinates) current-owner)]
     (swap! board-cursor assoc :board new-board)))
 
+(defn- set-active-board [index]
+  (swap! app-state assoc :active-board (get-in @app-state [:boards index :board])))
+
+(defn is-active? [board]
+  (let [active-board (:active-board @app-state)]
+    (or
+      (nil? active-board)
+      (= board active-board))))
+
 (defn on-cell-click [index board-cursor]
-  (do
-    (change-cell-owner index board-cursor)
-    (change-current-owner)))
+  (let [board-record (:board @board-cursor)]
+    (when (is-active? board-record)
+      (change-cell-owner index board-cursor)
+      (change-current-owner)
+      (set-active-board index))))
+
