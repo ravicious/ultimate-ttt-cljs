72417f901a7be5f1aff152a4141821215e708b0a
Rafał Cieślak <ravicious@gmail.com>
2015-04-04 11:43:23 +0200

    ui.helpers: more accurate `board-active?`

--- src/ultimate_ttt/ui/helpers.cljs
+++ src/ultimate_ttt/ui/helpers.cljs
@@ -1,14 +1,17 @@
-(ns ultimate-ttt.ui.helpers)
+(ns ultimate-ttt.ui.helpers
+  (:require [ultimate-ttt.game.referee :as referee]))
 
 (defn cell-active? [board cell-index]
   (= 0 (get-in board [:cells cell-index])))
 
-(defn board-active? [active-board-index board-index]
-  (or
-    (nil? active-board-index)
-    (= active-board-index board-index)))
+(defn board-active? [active-board-index board-index board]
+  (and
+    (or
+      (nil? active-board-index)
+      (= active-board-index board-index))
+    (not (referee/find-winner board))))
 
 (defn board-and-cell-active? [active-board-index board-index board cell-index]
   (and
-    (board-active? active-board-index board-index)
+    (board-active? active-board-index board-index board)
     (cell-active? board cell-index)))
