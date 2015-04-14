0a0f4dbbb675e67e29dd623050b97b03dd51d972
Rafał Cieślak <ravicious@gmail.com>
2015-04-14 18:02:02 +0200

    Property tests for referee/find-winner function
    Also, removed old unit tests & example property tests

--- test/ultimate_ttt/game/board_test.cljs
+++ test/ultimate_ttt/game/board_test.cljs
@@ -1,14 +1,9 @@
 (ns ultimate-ttt.game.board-test
   (:require-macros [cemerick.cljs.test
-                    :refer (is deftest with-test run-tests testing test-var)]
-                   [clojure.test.check.clojure-test
-                    :refer (defspec)])
+                    :refer (is deftest with-test run-tests testing test-var)])
   (:require [cemerick.cljs.test :as t]
             [ultimate-ttt.game.board :as b]
-            [ultimate-ttt.game.referee :as referee]
-            [clojure.test.check.generators :as gen]
-            [clojure.test.check.properties :as prop :include-macros true]
-            [clojure.test.check.clojure-test :as ct]))
+            [ultimate-ttt.game.referee :as referee]))
 
 ;;; Here we modify the cells so that each value is equal to its index.
 ;;; It makes testing get-cell method easier.
@@ -52,64 +47,3 @@
   (is (= '(1 1) (b/calculate-coordinates board 4)))
   (is (= '(2 0) (b/calculate-coordinates board 6))))
 
-(defn ascending?
-  [coll]
-  (every? (fn [[a b]] (<= a b))
-          (partition 2 1 coll)))
-
-(defspec testing-out-double-check
-  100
-  (prop/for-all [v (gen/vector gen/int)]
-                (let [s (sort v)]
-                  (and (= (count v) (count s))
-                       (ascending? s)))))
-
-;; rozmiar boardu (strictly positive int)
-;; index (choose 0 rozmiar-boardu)
-;; owner (one-of [1 2])
-;; (size, [index, owner], [index, owner], [index, owner])
-
-; (def board-gen (gen/fmap b/init-board size-gen))
-
-(def owners-gen (gen/shuffle [1 2]))
-
-(def size-gen (gen/choose 2 6))
-(def board-arguments-gen
-  ^{:doc "Returns a tuple containing the board size,
-         shuffled list of owners to cycle and a list of moves"}
-  (gen/bind size-gen
-            #(gen/tuple
-               (gen/return %)
-               (gen/shuffle [1 2])
-               (gen/fmap vec
-                         (gen/fmap distinct
-                                   (gen/vector
-                                     (gen/tuple
-                                       (gen/choose 0 (dec %))
-                                       (gen/choose 0 (dec %)))))))))
-
-(defn make-move [board [[x y] owner]]
-  (b/set-cell board x y owner))
-
-(defn valid-move? [board x y]
-  (and
-    (= 0 (b/get-cell board x y))
-    (not (referee/find-winner board))))
-
-(defn make-moves [board moves-and-owners]
-  (if-let [[[x y] owner] (first moves-and-owners)]
-    (if (valid-move? board x y)
-      (recur (b/set-cell board x y owner) (rest moves-and-owners))
-      board)
-    board))
-
-(def board-gen (gen/fmap (fn [[size owners moves]]
-                           (let [board (b/init-board size)
-                                 moves-and-owners (map vector moves (cycle owners))]
-                             (make-moves board moves-and-owners)))
-                         board-arguments-gen))
-
-
-(def board-full-gen (gen/tuple size-gen ))
-
-(def my-gen (gen/vector (gen/tuple (gen/choose 0 6) (gen/choose 7 9))))
--- test/ultimate_ttt/game/referee_test.cljs
+++ test/ultimate_ttt/game/referee_test.cljs
@@ -1,37 +1,69 @@
 (ns ultimate-ttt.game.referee-test
   (:require-macros [cemerick.cljs.test
-                    :refer (is deftest with-test run-tests testing test-var)])
+                    :refer (is deftest with-test run-tests testing test-var)]
+                   [clojure.test.check.clojure-test
+                    :refer (defspec)])
   (:require [cemerick.cljs.test :as t]
             [ultimate-ttt.game.board :as b]
-            [ultimate-ttt.game.referee :as r]))
+            [ultimate-ttt.game.referee :as r]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop :include-macros true]
+            [clojure.test.check.clojure-test :as ct]))
 
-(def board-won-by-player-1
-  (let [new-board (b/init-board)]
-    (->
-     new-board
-     (b/set-cell 0 0 1)
-     (b/set-cell 1 1 1)
-     (b/set-cell 2 2 1))))
+;;; Helpers & generators for tests
 
-(def board-won-by-player-2
-  (let [new-board (b/init-board)]
-    (->
-     new-board
-     (b/set-cell 0 2 2)
-     (b/set-cell 1 2 2)
-     (b/set-cell 2 2 2))))
+(def random-board-gen (gen/fmap (fn [cells]
+                                  (let [board (b/init-board)]
+                                    (assoc board :cells cells)))
+                                (gen/vector (gen/choose 0 2) 9)))
 
-(def unfinished-board
-  (let [new-board (b/init-board)]
-    (->
-     new-board
-     (b/set-cell 0 0 1)
-     (b/set-cell 1 2 2)
-     (b/set-cell 0 2 1))))
+(def rows {:horizontal [[0 1 2] [3 4 5] [6 7 8]]
+           :vertical [[0 3 6] [1 4 7] [2 5 8]]
+           :diagonal [[0 4 8] [2 4 6]]})
 
-(deftest find-winner
-  (testing "finding a winner for a won board"
-    (is (= 1 (r/find-winner board-won-by-player-1)))
-    (is (= 2 (r/find-winner board-won-by-player-2))))
-  (testing "finding a winner for an unfinished board"
-    (is (= nil (r/find-winner unfinished-board)))))
+(defn row->owners [board indexes]
+  (map #(b/get-cell board %) indexes))
+
+(defn rows->owners [board rows]
+  (->>
+    rows
+    (map #(row->owners board %))))
+
+(defn any-consecutive? [board dimension]
+  (->>
+    (dimension rows)
+    (rows->owners board)
+    (some #{[1 1 1] [2 2 2]})))
+
+;;; Actual tests
+
+(defspec board-with-less-than-three-marks-has-no-winner
+  100
+  (prop/for-all [board random-board-gen]
+                (let [freqs (frequencies (:cells board))]
+                  (if (and
+                          (< (freqs 1) 3)
+                          (< (freqs 2) 3))
+                    (not (r/find-winner board))
+                    true))))
+
+(defspec board-with-three-horizontal-marks-has-a-winner
+  100
+  (prop/for-all [board random-board-gen]
+                (if (any-consecutive? board :horizontal)
+                  (r/find-winner board)
+                  true)))
+
+(defspec board-with-three-vertical-marks-has-a-winner
+  100
+  (prop/for-all [board random-board-gen]
+                (if (any-consecutive? board :vertical)
+                  (r/find-winner board)
+                  true)))
+
+(defspec board-with-three-diagonal-marks-has-a-winner
+  100
+  (prop/for-all [board random-board-gen]
+                (if (any-consecutive? board :diagonal)
+                  (r/find-winner board)
+                  true)))
